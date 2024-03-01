e005874218e03148816a7acbebeb281d44245904
yanagitani <takanori.yanagitani@gmail.com>
2024-03-02 07:18:22 +0900

    feat: simple scripts

---
+++ gen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+which protoc | fgrep -q protoc || exec sh -c 'echo protoc missing.; exit 1'
+which protoc-gen-go | fgrep -q protoc-gen-go || exec sh -c 'echo protoc-gen-go missing.; exit 1'
+
+protodir="./reqs2http-proto"
+
+find \
+	"${protodir}" \
+	-type f \
+	-name '*.proto' |
+	xargs \
+		protoc \
+		--proto_path=reqs2http-proto \
+		--go_out=. \
+		--go_opt=paths=source_relative \
