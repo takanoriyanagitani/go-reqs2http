991e18ced5342faa19db7166469ad2bd63b0917a
yanagitani <takanori.yanagitani@gmail.com>
2024-03-04 10:05:08 +0900

    feat: unknown change source

--- buffered/change.go
+++ buffered/change.go
@@ -16,3 +16,20 @@ const (
 type ChangeSource interface {
 	Change(context.Context) (ChangeState, error)
 }
+
+type ChangeSrcFn func(context.Context) (ChangeState, error)
+
+func (f ChangeSrcFn) Change(c context.Context) (ChangeState, error) {
+	return f(c)
+}
+
+func (f ChangeSrcFn) AsIf() ChangeSource { return f }
+
+func ChangeSrcFnStaticNew(s ChangeState) ChangeSrcFn {
+	return ChangeSrcFn(func(_ context.Context) (ChangeState, error) {
+		return s, nil
+	})
+}
+
+var ChangeSrcFnUnknown ChangeSrcFn = ChangeSrcFnStaticNew(ChangeUnknown)
+var ChangeSourceUnknown ChangeSource = ChangeSrcFnUnknown.AsIf()
