f00875c01b547c2ab2e5d9a10a11729b7ae61f9c
yanagitani <takanori.yanagitani@gmail.com>
2024-03-04 09:25:21 +0900

    feat: usage source unknown

--- buffered/usage.go
+++ buffered/usage.go
@@ -16,3 +16,20 @@ const (
 type UsageSource interface {
 	Usage(context.Context) (UsageState, error)
 }
+
+type UsageSrcFn func(context.Context) (UsageState, error)
+
+func (f UsageSrcFn) Usage(ctx context.Context) (UsageState, error) {
+	return f(ctx)
+}
+
+func (f UsageSrcFn) AsIf() UsageSource { return f }
+
+func UsageSrcFnStaticNew(s UsageState) UsageSrcFn {
+	return UsageSrcFn(func(_ context.Context) (UsageState, error) {
+		return s, nil
+	})
+}
+
+var UsageSrcFnUnknown UsageSrcFn = UsageSrcFnStaticNew(UsageUnknown)
+var UsageSourceUnknown UsageSource = UsageSrcFnUnknown.AsIf()
