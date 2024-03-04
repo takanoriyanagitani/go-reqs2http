fd89f0853776ba525a7f20e83e5d1ed2ff03b132
yanagitani <takanori.yanagitani@gmail.com>
2024-03-04 10:05:26 +0900

    feat: nop sender

--- buffered/sender.go
+++ buffered/sender.go
@@ -1,7 +1,39 @@
 package buffered
 
+import (
+	"context"
+
+	rhp "github.com/takanoriyanagitani/go-reqs2http/reqs2http/v1"
+)
+
 type Sender interface {
 	UsageSource
 	ChangeSource
 	Buffer
+}
+
+type SenderFn struct {
+	UsageFn  UsageSrcFn
+	ChangeFn ChangeSrcFn
+	BufferFn BufFn
+}
+
+func (f SenderFn) Usage(ctx context.Context) (UsageState, error) {
+	return f.UsageFn(ctx)
+}
+
+func (f SenderFn) Change(ctx context.Context) (ChangeState, error) {
+	return f.ChangeFn(ctx)
+}
+
+func (f SenderFn) Push(ctx context.Context, q *rhp.Request) error {
+	return f.BufferFn(ctx, q)
+}
+
+func (f SenderFn) AsIf() Sender { return f }
+
+var SenderNop Sender = SenderFn{
+	UsageFn:  UsageSrcFnUnknown,
+	ChangeFn: ChangeSrcFnUnknown,
+	BufferFn: BufFnNop,
 }
