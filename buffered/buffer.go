e6d76af9d10da06c853dc78e694ec5d3de311c3f
yanagitani <takanori.yanagitani@gmail.com>
2024-03-04 10:04:56 +0900

    feat: nop buffer

--- buffered/buffer.go
+++ buffered/buffer.go
@@ -9,3 +9,14 @@ import (
 type Buffer interface {
 	Push(context.Context, *rhp.Request) error
 }
+
+type BufFn func(context.Context, *rhp.Request) error
+
+func (f BufFn) Push(c context.Context, q *rhp.Request) error { return f(c, q) }
+func (f BufFn) AsIf() Buffer                                 { return f }
+
+var BufFnNop BufFn = BufFn(func(_ context.Context, _ *rhp.Request) error {
+	return nil
+})
+
+var BufferNop Buffer = BufFnNop.AsIf()
